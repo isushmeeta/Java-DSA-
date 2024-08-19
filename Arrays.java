59adf97fbb6a6616bd274ea5aea3df7e5cc08874
isushmeeta <sushmitah0199@gmail.com>
2024-08-19 13:34:46 +0600

    arrays

--- Arrays.java
+++ Arrays.java
@@ -18,7 +18,8 @@ public class Arrays{
             }//taking input from users
 
         
-        
+        // Declaring array literal 
+        //int[] intArray = new int[]{ 1,2,3,4,5,6,7,8,9,10 };
 
 
           for (int i=0;i<size;i++){
