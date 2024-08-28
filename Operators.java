25b17d8e68757cbc279cca15ed355ecdbbf92c1b
isushmeeta <sushmitah0199@gmail.com>
2024-08-28 15:25:51 +0600

    Operators in Java

---
+++ Operators.java
@@ -0,0 +1,18 @@
+public class Operators {
+    public static void main(String args[]){
+
+        //Bitwiswe operators
+        //& Binary AND
+        // | Binary OR
+        // Binary XOR ^
+        // Binary Complements ~
+        // Binary Left shift <<
+        // Binary right shift >>
+        //Logical
+        // AND &&
+        // OR ||
+        // Not !
+        //A-=B means A=A-B
+    }
+    
+}
