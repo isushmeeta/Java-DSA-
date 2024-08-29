b93af6e513acc331cd29201e1c78743084c26f50
isushmeeta <sushmitah0199@gmail.com>
2024-08-29 20:42:57 +0600

    quick sort

---
+++ QuickSort.java
@@ -0,0 +1,51 @@
+//Time Complexcity O(n^2) worst case occurs 
+//when the pivot is always the lowest or the highest element
+//means either completely sorted or unsorted arrays
+//it doesnt require extra space like merge sort
+//average case of quicksort is O(nlogn)
+
+public class QuickSort {
+
+    public static int partition(int arr[], int low, int high){
+        int pivot= arr[high];
+        int i =low-1;
+        for (int j=low;j<high;j++){
+            if (arr[j]<pivot){
+                i++;
+                //swap
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+
+        }
+        i++;
+        int temp=arr[i];
+        arr[i]=pivot;
+        arr[high]=pivot;
+        return i;
+    }
+    public static void  quickSort(int arr[],int low,int high){
+        if (low<high){
+            int pivotIndex= partition(arr,low,high);
+
+            quickSort(arr, low, pivotIndex-1);
+            quickSort(arr, pivotIndex+1, high);
+        }
+
+
+    }
+
+    public static void main(String args[]){
+        int arr[]={6,3,9,5,2,8};
+        int n=arr.length;
+
+        quickSort(arr, 0, n-1);
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    
+}
--- SelectionSort.java
+++ SelectionSort.java
@@ -1,13 +1,31 @@
+//Time Complexcity is O(n^2) as there are two nexted loops
+//works well with small datasets
+//In place algorithm as it doesnt require extra space
+
 public class SelectionSort {
-    public static void printArray(int arr[]){
-        for (int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+    public static void printArray(int arr2[]){
+        for (int i=0;i<arr2.length;i++){
+            System.out.print(arr2[i]+" ");
         }
         System.out.println();
-        }
+    }
 
-        public static void main(String arr[]){
-            
+    public static void main(String arr[]){
+        int arr2[]={8,12,15,9,4,1};
+        //Selection Sort
+        for (int i=0;i<arr.length-1;i++){
+            for (int j=i+1;j<arr2.length;j++){
+                if (arr2[j]<arr2[i]){
+                    int temp= arr2[i];
+                    arr2[j]= arr2[j+1];
+                    arr2[j]= temp;
+                }
+            }
         }
+    printArray(arr2);
+
+
+            
+    }
     
 }
