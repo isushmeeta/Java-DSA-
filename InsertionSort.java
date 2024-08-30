8130ff49765c1940ad0e2fbf5160e908bcb61998
isushmeeta <sushmitah0199@gmail.com>
2024-08-30 15:20:47 +0600

    Complete code of insertion

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,3 +1,4 @@
+//time complexcity O(n^2)
 public class InsertionSort {
     public static void printArray(int arr[]){
         for (int i=0;i<arr.length;i++){
@@ -5,6 +6,27 @@ public class InsertionSort {
         }
         System.out.println();
 
+    }
+
+    public static void main(String arg[]){
+        int arr[]={7,8,3,1,2};
+
+
+        //insertion sort
+        for(int i=0;i<arr.length;i++){
+            int current=arr[i];
+            int j=i-1;
+            while(j>=0 && current< arr[j]){
+                arr[j+1]=arr[j];
+                j--;
+            }
+
+            //placement
+            arr[j+1]=current;
+
+        }
+
+        printArray(arr);
     }
 
     
