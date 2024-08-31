870eb5c1b7b5b7aeb19f2e2355341959008cbff5
isushmeeta <sushmitah0199@gmail.com>
2024-08-31 17:33:46 +0600

    Largest 3 elements

--- largestThreeElements.java
+++ largestThreeElements.java
@@ -2,15 +2,28 @@ public class largestThreeElements {
 
 
     public static void largestelems(int[]arr){
-        for (int i=0; i<arr.length; i++){
-            if (arr[i]> arr [i+i]){
-                System.out.println(arr[i]);
-
-            }else{
-                System.out.println("No large Number");
-
-            }
+        int first, second, third;
+        if (arr.length<3){
+            System.out.print("Invalid Input");
         }
+        third=first=second=Integer.MIN_VALUE;
+
+        for (int i=0; i<arr.length; i++){
+            if (arr[i]> first){
+                third=second;
+                second=first;
+                first=arr[i];
+
+            }else if(arr[i]>second){
+                third=second;
+                second=arr[i];
+            }else if(arr[i]>third){
+                third=arr[i];
+            }
+
+        
+        }
+        System.out.println(first +" " + second +" "+third);
     }
 
 
