6f66ec33c0fadccf563d9cbd9abc6ffe4c273b4d
isushmeeta <sushmitah0199@gmail.com>
2024-10-01 22:15:44 +0600

    String Reverse using recursion

---
+++ stringReverse.java
@@ -0,0 +1,23 @@
+//time complexcity O(n)  where n is the length of the string
+
+public class stringReverse {
+
+    public static void  printRev( String str, int idx ){
+
+        if(idx==0){
+            System.out.println(str.charAt(idx));
+            return;
+        }
+
+        System.out.print(str.charAt(idx));
+        printRev(str, idx-1);
+
+    } 
+
+    public static void main(String args[]){
+        String str="abcd";
+        printRev(str,str.length()-1);
+
+    }
+    
+}
