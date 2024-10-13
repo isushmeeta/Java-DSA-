edd09bfd5f34936c215d368d2951ce1f66883a05
isushmeeta <sushmitah0199@gmail.com>
2024-10-13 23:28:33 +0600

    added comment for insertion sort

--- InsertionSort.java
+++ InsertionSort.java
@@ -13,10 +13,10 @@ public class InsertionSort {
 
 
         //insertion sort
-        for(int i=0;i<arr.length;i++){
+        for(int i=0;i<arr.length;i++){//unsorted array te loop run
             int current=arr[i];
-            int j=i-1;
-            while(j>=0 && current< arr[j]){
+            int j=i-1;//sorted part er last index
+            while(j>=0 && current< arr[j]){//sorted part traverse
                 arr[j+1]=arr[j];
                 j--;
             }
